2fd5e2f56c542fe7a9a5d0be5402f66431d0c2f0
Future, Inc <vidbitfuture@gmail.com>
2017-03-30 20:37:43 -0400

    Update subcount.php
    Fixed a stupid mistake...

--- subcount.php
+++ subcount.php
@@ -1,5 +1,5 @@
 <?php
-$_GET['userid'] = $userid;
+$userid = $_GET['userid'];
 $userinfojson = file_get_contents('https://api.vidbitfuture.co/userinfo.php?id='.$userid);
 $userinfo = json_decode($userinfojson,true);
 echo $userinfo['subcount'];
